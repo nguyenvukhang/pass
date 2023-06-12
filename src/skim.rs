9a030b0d36fc018ebaa1af1e53c6887f2120a786
nguyenvukhang <brew4k@gmail.com>
2023-06-12 09:43:50 +0800

    feat: skim::select_one

--- src/skim.rs
+++ src/skim.rs
@@ -14,13 +14,25 @@ impl SkimItem for Name {
     }
 }
 
+/// Use skim to select a choice in a list of strings
+pub fn select_one<I: Iterator<Item = String>>(choices: I) -> Option<String> {
+    let opts = SkimOptionsBuilder::default().height(Some("10")).build().ok()?;
+    let (tx, rx): (SkimItemSender, SkimItemReceiver) = unbounded();
+
+    choices.for_each(|v| {
+        let _ = tx.send(Arc::new(Name(v)));
+    });
+
+    drop(tx); // so that skim could know when to stop waiting for more items.
+
+    Skim::run_with(&opts, Some(rx))
+        .and_then(|mut out| out.selected_items.pop())
+        .map(|v| v.output().to_string())
+}
+
 pub fn skim(db: Database) {
-    let options = SkimOptionsBuilder::default()
-        .height(Some("10"))
-        .multi(false)
-        .no_clear(false)
-        .build()
-        .unwrap();
+    let options =
+        SkimOptionsBuilder::default().height(Some("10")).build().unwrap();
 
     let (tx_item, rx_item): (SkimItemSender, SkimItemReceiver) =
         skim::prelude::bounded(db.count());
